b98251460bbb55c55460bd3b44ccb233828a5dab
valhallajazzy <valhallajazzy@mail.ru>
2024-02-22 00:40:30 +0600

    download book covers

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import os
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
+from urllib.parse import urljoin, urlsplit, unquote
 
 
 def check_for_redirect(response):
@@ -23,7 +24,7 @@ def create_path_to_file(number, folder='books/'):
     return path
 
 
-def download_books():
+def download_txt():
     for number in range(1, 11):
         url = f'https://tululu.org/txt.php?id={number}'
         response = requests.get(url)
@@ -37,5 +38,27 @@ def download_books():
             continue
 
 
+def download_picture(folder='pictures'):
+    for number in range(1, 11):
+        os.makedirs(f'{folder}', exist_ok=True)
+        url = f'https://tululu.org/b{number}'
+        response = requests.get(url)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.text, 'lxml')
+        try:
+            url = urlsplit(url)
+            path = unquote(soup.find('table', class_='d_book').find('img')['src'])
+            pic_extension = path.split('.')[1]
+            base_url = url.scheme + '://' + url.netloc
+            picture_link = urljoin(base_url, path)
+            img_response = requests.get(picture_link)
+            img_response.raise_for_status()
+            with open(f"{folder}/{number}.{pic_extension}", 'wb') as file:
+                file.write(img_response.content)
+        except AttributeError:
+            continue
+
+
 if __name__ == '__main__':
-    download_books()
+    download_txt()
+    download_picture()
