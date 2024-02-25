891050222967631751aefeb8602270c9e6509ed9
valhallajazzy <valhallajazzy@mail.ru>
2024-02-25 20:47:41 +0600

    Refactor tululu.py

--- tululu.py
+++ tululu.py
@@ -2,45 +2,41 @@ import requests
 import os
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
-from urllib.parse import urljoin, urlsplit, unquote
+from urllib.parse import urljoin, unquote
 import argparse
+import logging
+from time import sleep
+
+
+class RedirectException(Exception):
+    pass
 
 
 def check_for_redirect(response):
     if response.history:
-        raise requests.HTTPError("Произошел redirect")
+        raise RedirectException("Произошел redirect")
 
 
 def parse_book_page(soup):
-    try:
+    if soup.find('div', id='content') is not None:
         author_and_title = soup.find('div', id='content').find('h1').text.split(' \xa0 :: \xa0 ')
         title = sanitize_filename(author_and_title[0].strip())
         author = sanitize_filename(author_and_title[1].strip())
-        genres = []
         links_with_genres = soup.find('span', class_='d_book').find_all('a')
-        for link in links_with_genres:
-            genres.append(link.text)
+        genres = [link.text for link in links_with_genres]
+        img_path = unquote(soup.find('table', class_='d_book').find('img')['src'])
         divs_with_comments = soup.find_all('div', class_='texts')
         comments = []
         for div in divs_with_comments:
             comment = div.find('span')
             if comment:
                 comments.append(comment.text)
-        return {
-                'title': title,
+
+        return {'title': title,
                 'author': author,
                 'genres': genres,
-                'comments': comments
-                }
-    except AttributeError:
-        return None
-
-
-def get_page(book_id):
-    url = f'https://tululu.org/b{book_id}'
-    response = requests.get(url)
-    response.raise_for_status()
-    return response
+                'comments': comments,
+                'img_path': img_path}
 
 
 def create_path_to_file(book_id, title, folder='books/'):
@@ -52,10 +48,9 @@ def create_path_to_file(book_id, title, folder='books/'):
 
 def download_picture(book_id, soup, folder='pictures'):
     os.makedirs(f'{folder}', exist_ok=True)
-    url = urlsplit(f'https://tululu.org/b{book_id}')
-    path = unquote(soup.find('table', class_='d_book').find('img')['src'])
+    path = parse_book_page(soup)['img_path']
     pic_extension = path.split('.')[1]
-    base_url = url.scheme + '://' + url.netloc
+    base_url = f'https://tululu.org/b{book_id}'
     picture_link = urljoin(base_url, path)
     img_response = requests.get(picture_link)
     img_response.raise_for_status()
@@ -63,23 +58,37 @@ def download_picture(book_id, soup, folder='pictures'):
         file.write(img_response.content)
 
 
-def download_books(start_id, end_id):
-    for book_id in range(start_id, end_id + 1):
-        url = f'https://tululu.org/txt.php?id={book_id}'
-        response = requests.get(url)
-        response.raise_for_status()
-        try:
-            check_for_redirect(response)
-            book_page_response = get_page(book_id)
-            soup = BeautifulSoup(book_page_response.text, 'lxml')
-            book_info = parse_book_page(soup)
-            filepath = create_path_to_file(book_id, book_info['title'], folder='books/')
-            with open(f"{filepath}", 'w') as file:
-                file.write(response.text)
-            download_picture(book_id, soup)
-            print(f"Название: {book_info['title']} \nАвтор: {book_info['author']} \n")
+def download_book(book_id):
+    url_text = f'https://tululu.org/txt.php'
+    params = {'id': book_id}
+    text_response = requests.get(url_text, params=params)
+    text_response.raise_for_status()
+    check_for_redirect(text_response)
+    url_book_page = f'https://tululu.org/b{book_id}'
+    book_page_response = requests.get(url_book_page)
+    book_page_response.raise_for_status()
+    soup = BeautifulSoup(book_page_response.text, 'lxml')
+    about_book = parse_book_page(soup)
+    filepath = create_path_to_file(book_id, about_book['title'], folder='books/')
+    with open(f"{filepath}", 'w') as file:
+        file.write(text_response.text)
+    download_picture(book_id, soup)
+    print(f"Название: {about_book['title']} \nАвтор: {about_book['author']} \n")
 
-        except requests.HTTPError:
+
+def download_books_in_range(start_id, end_id):
+    for book_id in range(start_id, end_id + 1):
+        try:
+            download_book(book_id)
+        except requests.exceptions.ConnectionError:
+            print('Ошибка интернет-соединения, переподключаюсь')
+            sleep(5)
+        except requests.exceptions.HTTPError:
+            print(f'Ошибка http-подключения к серверу с id книги - {book_id}, переподключаюсь')
+            sleep(5)
+        except RedirectException:
+            logging.warning(f'На странице с ожидаемым id книги - {book_id}, '
+                            f'обложки не сущетсвует. Картинка не скачана.')
             continue
 
 
@@ -87,7 +96,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Данная программа скачивает книги с сайта https://tululu.org/ в указанном диапазоне id'
     )
-    parser.add_argument('start_id', help='id книги с которой начнется скачивание', type=int)
-    parser.add_argument('end_id', help='id книги на которой окончится скачивание', type=int)
+    parser.add_argument('-s', '--start_id', help='id книги с которой начнется скачивание',
+                        default=1, type=int)
+    parser.add_argument('-e', '--end_id', help='id книги на которой окончится скачивание',
+                        default=10, type=int)
     args = parser.parse_args()
-    download_books(args.start_id, args.end_id)
+    download_books_in_range(args.start_id, args.end_id)
